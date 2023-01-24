532f2e0fcda347c8a0b412709ca3b0cd6a3948ee
nithish26s <122864433+nithish26s@users.noreply.github.com>
2023-01-24 12:04:34 +0530

    Add files via upload

---
+++ Datatype_2.py
@@ -0,0 +1,35 @@
+# a=None
+# print(type(a))
+
+# b=[0,4.6,[7,8.5],"hi"]
+# print(type(b))
+# print(b)
+# print(b[0],type(b[0]))
+# print(b[1],type(b[1]))
+# print(b[2],type(b[2]))
+# print(b[3],type(b[3]))
+# print(b[4]) # Error
+# print(b[2][0],type(b[2][0]))
+# print(b[2][1],type(b[2][1]))
+# c=[]
+# print(c,type(c))
+# v=[3,6]
+# v[0]=8
+# print(v)
+
+# a=3,4,5,6
+# print(a,type(a))
+
+# a,b,c=2,3,6
+# print(type(a))
+# print(type(a),type(b),type(c))
+
+# b=4,7.9,'hi',[3,2],(6,7),None
+# print(b,type(b))
+# print(type(b[0]))
+# print(type(b[3]))
+# print(type(b[4]))
+
+# v=(3,6)
+# print(type(v))
+# v[0]=8  # TypeError: does not support item assignment
